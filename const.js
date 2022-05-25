d6679fcfd2bce2782015a7dfa3b3c91d7f42e1dc
leeladhar <pleeladharp@gmail.com>
2022-05-25 14:46:58 +0530

    updated

---
+++ const.js
@@ -0,0 +1,16 @@
+
+
+var i= "yes";
+let b= "not";
+const c="king";
+
+function a(){
+   console.log(i);
+   console.log(b);
+   console.log(c);
+
+}
+a();
+console.log(b);
+console.log(b);
+console.log(c);
