5a1eb20371dcb4411d0ea8fba2a430ffac1e12f7
LeeladharAngular <105802332+LeeladharAngular@users.noreply.github.com>
2022-05-26 19:55:29 +0530

    hw
    object creation using new key word and literals

---
+++ ap.js
@@ -0,0 +1,109 @@
+var car = new Object();
+
+car.color = "blue";
+car.brand = "Maruthi suziki";
+car.name=  "swift dezire"
+car.coolent= "oil"                      
+car["Milage"] = "22";
+console.log(car.color);
+console.log(car.brand);
+console.log(car.name);
+console.log(car.coolent);
+console.log(car["Milage"]);
+
+var car={
+color  : "blue",
+brand  : "Maruthi suziki",
+name   :  "swift dezire",
+coolent: "oil" ,                     
+engineCondition :"Good"
+}
+console.log(car);
+
+
+var bike=new Object();
+bike["enginecepacity"]="2.5leters";
+bike.cc="2500";
+bike.tark="3000";
+bike.modal="2022";
+bike.name="ducati diaveal";
+console.log(bike["enginecepacity"]);
+console.log(bike.cc);
+console.log(bike.tark);
+console.log(bike.modal);
+console.log(bike.name);
+
+var bike={
+    enginecepacity:"2.5leters",
+    cc   :"2500",
+    tark :"3000",
+    modal:"2022",
+    name :"ducati diaveal"
+}
+console.log(bike);
+ 
+
+var pen= new Object();
+pen.color="blue";
+pen.type = "marker";
+pen.name = "camlin";
+pen.grip ="regular";
+pen["gripType"]="elastic";
+console.log(pen.color);
+console.log(pen.type);
+console.log(pen.name);
+console.log(pen.grip);
+console.log(pen["gripType"]);
+
+var pen={
+    color:"blue",
+    type : "marker",
+    name : "camlin",
+    grip :"regular",
+    gripType:"elastic"    
+}
+console.log(pen);
+ 
+
+var shoes=new Object();
+shoes.brand="Reebook";
+shoes.gender="men";
+shoes.size=11;
+shoes.color="white";
+shoes["type"]="generic";
+console.log(shoes.brand);
+console.log(shoes.gender);
+console.log(shoes.size);
+console.log(shoes.white);
+console.log(shoes["type"]);
+  
+var shoes={
+brand:"Reebook",
+gender:"men",
+size:"11",
+color:"white",
+type:"generic"
+}
+console.log(shoes);
+
+
+var fan=new Object();
+fan.brand="bajaj";
+fan.type="wallFan";
+fan.torque="400 N";
+fan.rpm="1500";
+fan["color"]="white";
+console.log(fan.brand);
+console.log(fan.type);
+console.log(fan.torque);
+console.log(fan.rpm);
+console.log(fan["color"]);
+
+var fan={
+brand:"bajaj",
+type:"wallFan",
+torque:"400 N",
+rpm:"1500",
+color:"white",
+}
+console.log(fan);
