e09d7a155a1977b76c9403b26ac7e06fb1376e6c
leeladhar <pleeladharp@gmail.com>
2022-05-29 20:53:58 +0530

    we

--- const.js
+++ const.js
@@ -5,12 +5,15 @@ let b= "not";
 const c="king";
 
 function a(){
+   let d= "w"
    console.log(i);
    console.log(b);
    console.log(c);
+   console.log(d)
 
 }
 a();
 console.log(b);
 console.log(b);
 console.log(c);
+console.log(d);
