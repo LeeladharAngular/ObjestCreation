1894ed4678d8d9f3e6550afbd5e70bf7644acb96
leeladhar <pleeladharp@gmail.com>
2022-05-30 07:18:02 +0530

    lucky

--- const.js
+++ const.js
@@ -1,19 +1,18 @@
 
 
 var i= 1+2-3;
-let b= "not";
-const c="king";
+var b= "not";
+
 
 function a(){
    let d= "w"
    console.log(i);
    console.log(b);
-   console.log(c);
-   console.log(d)
+   
+   
 
 }
 a();
+console.log(i);
 console.log(b);
-console.log(b);
-console.log(c);
-console.log(d);
+
